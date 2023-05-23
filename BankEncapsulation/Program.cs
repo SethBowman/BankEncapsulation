d1b0a1cbf135cf375a1a1f2b7b5b86a4a18b24cd
Seth Bowman <brandonsethbowman@gmail.com>
2023-05-23 13:37:51 -0500

    Exercise completed

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            Console.WriteLine("Welcome to Bowman Banking App!");
+            Console.WriteLine("==============================");
+
+            while(true)
+            {
+                Console.WriteLine("\nPlease select an option:");
+                Console.WriteLine("1. Deposit money");
+                Console.WriteLine("2. Check balance");
+                Console.WriteLine("3. Exit");
+
+                int option;
+                if(!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid option.");
+                    continue;
+                }
+
+                switch(option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the amount to deposit:");
+                        double amount;
+                        if(!double.TryParse(Console.ReadLine(), out amount))
+                        {
+                            Console.WriteLine("Invalid input. Please enter a valid amount.");
+                            continue;
+                        }
+
+                        account.Deposit(amount);
+                        Console.WriteLine("Deposit successful!");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Current balance: {account.GetBalance()}");
+                        break;
+                    case 3:
+                        Console.WriteLine("Thank you for using Bowman Banking App!");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Please enter a valid option.");
+                        break;
+
+                }
+            }
         }
     }
 }
